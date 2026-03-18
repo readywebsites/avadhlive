24d774146615d93e1ad416e2dfbbce38ea6195b5
Mukund920 <mak7kapadia@gmail.com>
2026-03-18 16:10:39 +0530

    tiny-mcp-setting

--- backend/settings.py
+++ backend/settings.py
@@ -224,9 +224,17 @@ CKEDITOR_5_CONFIGS = {
 }
 
 TINYMCE_DEFAULT_CONFIG = {
-    "height": "500px",
-    "width": "100%",
+    'theme': "silver",
+    'branding': False,
+    "height": "320px",
+    "width": "960px",
     "menubar": "file edit view insert format tools table help",
-    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount",
-    "toolbar": "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
 }
