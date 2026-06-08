70d40eff6ad06aa5a15962c33a842b3c4864112e
readywebsites <biz499websites@gmail.com>
2026-06-08 13:56:31 +0530

    sitemap and robot url

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 from api.views import frontend
 
 urlpatterns = [
@@ -9,6 +10,8 @@ urlpatterns = [
     path('api/', include('api.urls')),  # Include URLs from our 'api' app
     path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
     re_path(r"^.*$", frontend),
 ]
 
